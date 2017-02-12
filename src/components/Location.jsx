1c40d32424fda04f33e0b542ba562665d1d4c79e
playahater <mishkez@gmail.com>
2017-02-12 18:54:44 +0100

    render geolocation

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -3,11 +3,42 @@ import * as RB from 'react-bootstrap';
 
 export default class Location extends Component {
 
+  constructor(props) {
+    super(props)
+    this.pos = undefined;
+    this.initLocation = this.initLocation.bind(this);
+  }
+
+  initLocation(){
+    navigator.geolocation.getCurrentPosition (
+      position => {
+        this.pos = position
+      },
+      error => {
+        console.log(error)
+      },
+      {
+        maximumAge: 300,
+        timeout: 5000,
+        enableHighAccuracy: true
+      }
+    );
+  }
+
   render() {
+
+    this.initLocation();
+
+    setTimeout(() => {
+      let elem = document.getElementById('geo');
+      elem.innerHTML = 'Longitude: ' + this.pos.coords.latitude + '<br />' + 'Latitude: ' + this.pos.coords.longitude + '<br />' + 'Accuracy: ' + this.pos.coords.accuracy;
+    }, 1000);
+
     return (
-      <div>
-        location text
+      <div id='geo'>
+        loading ..
       </div>
     );
+
   }
 }
